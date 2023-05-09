02257f216f3e7269819188e070c4ddf474b849d3
tmdmini99 <tmdals2451@naver.com>
2023-05-09 11:24:08 +0900

    setting base source
    처음 세팅입니다

---
+++ src/main/java/com/iu/base/GdjBoot52Application.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdjBoot52Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdjBoot52Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/iu/base/GdjBoot52ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iu.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdjBoot52ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
