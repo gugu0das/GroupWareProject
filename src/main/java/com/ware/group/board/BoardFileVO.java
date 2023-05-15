9aaca6fe58c28e26dddc9735764a72ebe50ab657
wuxinanzi <suu308@gmail.com>
2023-05-15 17:35:02 +0900

    BoardFIleVO : BoardFIleVO MODIFY
    BoardFIleVO : BoardFIleVO MODIFY

--- src/main/java/com/ware/group/board/BoardFileVO.java
+++ src/main/java/com/ware/group/board/BoardFileVO.java
@@ -3,34 +3,17 @@ package com.ware.group.board;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Getter
-//@Setter
+@Getter
+@Setter
 public class BoardFileVO {
 		
 	
-	private Long fileNum;
+	private Long id;
 	
 	private String fileName;
 	private String oriName;
 	
-	public Long getFileNum() {
-		return fileNum;
-	}
-	public void setFileNum(Long fileNum) {
-		this.fileNum = fileNum;
-	}
-	public String getFileName() {
-		return fileName;
-	}
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-	public String getOriName() {
-		return oriName;
-	}
-	public void setOriName(String oriName) {
-		this.oriName = oriName;
-	}
+	
 	
 	
 }
