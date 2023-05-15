e00269175b2b8540fad9ade4d147e1f565740952
kimtaehyeun <rlaxogus2799@naver.com>
2023-05-15 16:08:56 +0900

    com.ware.group.department : Department initSetting

---
+++ src/main/java/com/ware/group/department/DepartmentDAO.java
@@ -0,0 +1,11 @@
+package com.ware.group.department;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DepartmentDAO {
+
+	public List<DepartmentVO>getDepartmentList()throws Exception;
+}
