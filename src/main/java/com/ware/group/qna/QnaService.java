db3ed20858e2fc9a20e84a3840a0fe072048af12
wuxinanzi <suu308@gmail.com>
2023-05-18 12:12:56 +0900

    group.qna : qnaService MODIFY
    group.qna : qnaService MODIFY

--- src/main/java/com/ware/group/qna/QnaService.java
+++ src/main/java/com/ware/group/qna/QnaService.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.ware.group.board.BoardFileVO;
+import com.ware.group.notice.NoticeFileVO;
+import com.ware.group.notice.NoticeVO;
 import com.ware.group.util.FileManager;
 import com.ware.group.util.Pager;
 
@@ -29,4 +33,62 @@ public class QnaService {
 		
 		return qnaDAO.getList(pager);
 	}
+	public QnaVO getDetail(QnaVO qnaVO) throws Exception {
+		return qnaDAO.getDetail(qnaVO);
+		
+		
+	}
+
+	
+	public int setInsert(QnaVO qnaVO, MultipartFile [] multipartFiles) throws Exception {
+		int result = qnaDAO.setInsert(qnaVO);
+		
+//		Random random = new Random();
+//		int num = random.nextInt(1);
+//		
+//		if(num == 0) {
+//			throw new Exception();
+//		}
+		
+		if(multipartFiles != null) {
+			for(MultipartFile multipartFile : multipartFiles) {
+				
+				if(!multipartFile.isEmpty()) {
+					String fileName = fileManager.saveFile(path, multipartFile);
+					QnaFileVO qnaFileVO = new QnaFileVO();
+					qnaFileVO.setFileName(fileName);
+					qnaFileVO.setOriName(multipartFile.getOriginalFilename());
+					qnaFileVO.setId(qnaVO.getId());
+					System.out.println("힘들다");
+					result = qnaDAO.setQnaFileAdd(qnaFileVO);
+				}
+			}
+		}
+		
+		return result;
+	}
+
+	
+	public int setUpdate(QnaVO qnaVO,MultipartFile multipartFiles) throws Exception {
+		return qnaDAO.setUpdate(qnaVO);
+	}
+
+	
+	public int setDelete(QnaVO qnaVO) throws Exception {
+		return qnaDAO.setDelete(qnaVO);
+	}
+
+	
+	 
+	 public BoardFileVO getFileDetail(BoardFileVO boardFileVO) throws Exception { 
+		 return qnaDAO.getFileDetail(boardFileVO); 
+		 
+	 }
+	 // 조회수
+	 public int setQnaHit(QnaVO qnaVO)throws Exception{
+		
+		 return qnaDAO.setQnaHit(qnaVO);
+	 }
+	 
+	 
 }
