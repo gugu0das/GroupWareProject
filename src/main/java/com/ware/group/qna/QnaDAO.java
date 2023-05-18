59ca590ef0f736528361bd1d201db0698da49d8c
wuxinanzi <suu308@gmail.com>
2023-05-18 12:13:12 +0900

    group.qna : qnaDAO MODIFY
    group.qna : qnaDAO MODIFY

--- src/main/java/com/ware/group/qna/QnaDAO.java
+++ src/main/java/com/ware/group/qna/QnaDAO.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.ware.group.board.BoardFileVO;
+import com.ware.group.notice.NoticeFileVO;
+import com.ware.group.notice.NoticeVO;
 import com.ware.group.util.Pager;
 
 
@@ -19,5 +22,23 @@ public interface QnaDAO {
 	
 	// 글 하나 조회
 	public QnaVO getDetail(QnaVO qnaVO) throws Exception;
+	
+	// 글쓰기
+	public int setInsert(QnaVO qnaVO) throws Exception;
+	
+	// 글수정
+	public int setUpdate(QnaVO qnaVO) throws Exception;
+	
+	// 글삭제
+	public int setDelete(QnaVO qnaVO) throws Exception;
+	
+	// 파일 등록
+	public int setQnaFileAdd(QnaFileVO qnaFileVO) throws Exception;
+	
+	// 파일조회
+	public BoardFileVO getFileDetail(BoardFileVO boardFileVO) throws Exception;
+	
+	// 조회수
+	public int setQnaHit(QnaVO qnaVO) throws Exception;
 	
 }
