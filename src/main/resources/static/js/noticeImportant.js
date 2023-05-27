c6920e903adc3d6cdcdd6b47ce430fe1a5e4da0b
wuxinanzi <suu308@gmail.com>
2023-05-28 00:59:12 +0900

    js/noticeImportant.js add
    add 할때 중요 표시 해주면 색 and 고정 됨

---
+++ src/main/resources/static/js/noticeImportant.js
@@ -0,0 +1,35 @@
+$(".submitButton").on("click", function(){
+
+    if($("#important").prop("checked")) {
+        $("#important").val(1);
+    } else {
+        $("#important").val(0);
+    }
+})
+
+// $(document).ready(function() {
+//     $(".check-item").each(function() {
+//         if($(this).attr('data-num-important') == '1') {
+//             let $row = $(this).closest('tr').addClass("trfirst");
+//             $row.insertBefore($row.parent().find('tr:first').addClass("trfirst"));
+//             $row.find('.title').addClass("checkcolor");
+//             $(this).css("background-color", "yellow");
+//          }
+//      });
+//  });
+
+
+$(document).ready(function(){
+
+    $.ajax({
+        url: "/notice/importantList",
+        type: "GET",
+        data : {
+            important : 1
+        },
+        success: function(result) {
+            $(".importantList").html(result);
+        }
+    })
+
+})
