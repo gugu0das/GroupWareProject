7d0810402bfcae1274dac2cd836a8f0143a7e60d
tmdmini99 <tmdals2451@naver.com>
2023-06-05 15:31:20 +0900

    Approval : infromation paging

--- src/main/resources/static/js/approvalInformation.js
+++ src/main/resources/static/js/approvalInformation.js
@@ -1,16 +1,33 @@
-/*$(document).on("click",".dropbtn",function(){
-    let catId=$(this).attr("data-catId")
+$(document).on("click",".tabar",function(){
+    let catId=$(this).attr("id")
+    console.log("들어는 왔다");
         $.ajax({
-        type : "get",
+        type : "post",
         url : "./information",
         data :{
             categoryId : catId
         },
         success :function(result){
-            $("#allList").html(result)
+            $("#id_"+catId).html(result)
         },
         error:function(){
             console.log('error')
         }
     })
+})
+
+/*$(document).click(function(){
+	console.log("왜?");
+	console.log($("#dropdownMenuButton1").next().hasClass("show"));
+	if(!$("#dropdownMenuButton1").next().hasClass("show")){
+		$("#dropdownMenuButton1").next().removeClass("show");
+	}
+	$("#dropdownMenuButton1").click(function(){
+	
+	$(this).next().addClass("show");
+	
+})
 })*/
+
+
+
