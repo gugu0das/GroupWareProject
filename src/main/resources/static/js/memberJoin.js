fb9045dbac3156ecc754ce11dcb5136396b24efc
kimtaehyeun <rlaxogus2799@naver.com>
2023-06-17 16:32:18 +0900

    member.memberProfile.* : 프로필 데이터 추가

--- src/main/resources/static/js/memberJoin.js
+++ src/main/resources/static/js/memberJoin.js
@@ -20,3 +20,72 @@ $('#finishTime').timepicker({
     scrollbar: false
 });
 
+$("#idCheck").on("click", async function () {
+    let id = $("#accountId").val()
+    
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "/member/idDuplicateCheck",
+        data: {
+            employeeId: id,
+            accountId: id
+
+        },
+        success: function (response) {
+            Swal.fire({
+                title: 'ID 검증',
+                html: '중복 검사중입니다.',
+                timer: 800,
+                timerProgressBar: false,
+                didOpen: () => {
+                    Swal.showLoading()
+                    timerInterval = setInterval(() => {
+                    }, 100)
+                },
+                willClose: () => {
+                    clearInterval(timerInterval)
+                    if (response) {
+                        Swal.fire({
+                            icon: 'error',
+                            title: '중복된 사원번호입니다.',
+                            text: '다시 확인해주세요',
+
+                        }).then(result => {
+                            $("#accountId").removeClass("success")
+                            $("#accountId").addClass("fail")
+                        });
+                    }
+                    else {
+                        Swal.fire({
+                            icon: 'success',
+                            title: '사용 가능한 아이디입니다.',
+                            text: '아이디를 사용하시겠습니까?',
+                            showCancelButton: true,
+                            confirmButtonColor: '#3085d6',
+                            cancelButtonColor: '#d33',
+                            confirmButtonText: '예',
+                            cancelButtonText: '아니오'
+                        }).then(result => {
+                            if (result.isConfirmed) {
+                                $("#accountId").removeClass("fail")
+                                $("#accountId").addClass("success")
+                            }
+                        });
+                    }
+
+                },
+
+
+            }).then((result) => {
+            })
+        },
+        error: function () {
+            console.log("error")
+
+        }
+
+    })
+
+})
